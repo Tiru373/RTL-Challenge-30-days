22a88289fe9aed541a83d9d24392ad024ddc61a1
Tirumalasetty Ashok <147503242+Tiru373@users.noreply.github.com>
2024-03-25 17:33:44 +0530

    Create async_nbit_counter.v

---
+++ counters/async_nbit_counter.v
@@ -0,0 +1,15 @@
+module async_nbit_counter(
+    input wire clk, reset,
+    output reg [N-1:0] count
+);
+
+parameter N = 4; // Define the number of bits
+
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        count <= {N{1'b0}}; // Reset to 0
+    else
+        count <= count + 1; // Increment on each clock cycle
+end
+
+endmodule
